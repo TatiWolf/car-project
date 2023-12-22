812b4660fd32c397a9af8a82551ced17a9acd624
kyrgansky.ku <kyrgansky.ku@zdmail.com>
2023-12-22 15:38:32 +0300

    added all button and styles

--- src/slider.js
+++ src/slider.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
   let swiper = new Swiper(".mySwiper", {
     cssMode: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+      loop: true,
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
     mousewheel: true,
     keyboard: true,
